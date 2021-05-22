5f71c543108bcd84b4769b2879c3a074a43481a9
ldolenjashvili <lasha.dolenjashvili1994@gmail.com>
2021-05-22 09:51:37 +0400

    First Draft

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [
+    './public/**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'quicksand': ['"Quicksand"', 'sans-serif']
+      },
+      backgroundImage: theme => ({
+        'hero': "url('img/hero3.jpg')"
+       })
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
